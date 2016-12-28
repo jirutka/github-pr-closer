a8e5da5da932ea981a2966cda824f55896f1fb40
Jakub Jirutka <jakub@jirutka.cz>
2016-12-28 18:40:11 +0100

    Rewrite scripts, use venv and /bin/sh

---
+++ script/utils.sh
@@ -0,0 +1,19 @@
+# vim: set ts=4:
+
+die() {
+	# bold red
+	printf '\033[1;31mERROR:\033[0m %s\n' "$1" >&2
+	shift
+	printf '  %s\n' "$@"
+	exit 1
+}
+
+einfo() {
+	# bold cyan
+	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2
+}
+
+ewarn() {
+	# bold yellow
+	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
+}
